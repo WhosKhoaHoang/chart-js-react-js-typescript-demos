a0a613c3c180afcd33f5e48d04333cf696529cc0
Khoa Hoang <whoskhoahoang@gmail.com>
2024-02-02 21:43:13 +0900

    Implement doughnut component
    Note the chart: Chart<'doughnut'> type annotation.

---
+++ src/Doughnut.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Doughnut } from 'react-chartjs-2'
+
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  Chart
+} from 'chart.js'
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend
+)
+
+const DoughnutChart = () => {
+  const data = {
+    labels: ['Red', 'Blue'],
+    datasets: [
+      {
+        label: 'Shop 1',
+        data: [3,6],
+        backgroundColor: ['red', 'blue'],
+        borderColor: ['black', 'black'],
+        cutout: '90%'
+      }
+    ],
+  }
+
+  const options = {
+    aspectRatio: 2,
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  }
+
+  const doughnutLabel = {
+    id: 'doughnutLabel',
+    afterDatasetsDraw(chart: Chart<'doughnut'>) {
+      const { ctx, data } = chart
+      const centerX = chart.getDatasetMeta(0).data[0].x
+      const centerY = chart.getDatasetMeta(0).data[0].y
+      ctx.save()
+      ctx.font = 'bold 90px sans-serif'
+      ctx.fillStyle = 'black'
+      ctx.textAlign = 'center'
+      //ctx.baseAlign = 'middle'
+      ctx.fillText('Test', centerX, centerY)
+    }
+    
+  }
+
+  return (
+    <div>
+      <Doughnut
+        data={data}
+        options={options}
+        plugins={[doughnutLabel]}
+      />
+    </div>
+  )
+}
+
+export default DoughnutChart
